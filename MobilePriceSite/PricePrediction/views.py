7740d1def1605738b17fecac6ae12fdf652ea80d
Sasan <77465683+sasansabour@users.noreply.github.com>
2024-11-25 13:46:54 -0500

    Update views.py

--- MobilePriceSite/PricePrediction/views.py
+++ MobilePriceSite/PricePrediction/views.py
@@ -6,7 +6,7 @@ from .models import MobilePriceForm
 import joblib
 import numpy as np
 import pandas as pd
-#sklearn
+
 
 
 # AWS API endpoint
@@ -15,6 +15,7 @@ API_ENDPOINT = "https://prm6km3cwg.execute-api.us-east-2.amazonaws.com/Test/"
 # Create your views here.
 def index(request):
     result = None
+    finalResult = ""
 
     if request.method == "POST":
         #form = MobilePriceForm(request.POST)
@@ -83,7 +84,7 @@ def index(request):
 
         # Prepare the payload for the API
         formatted_input = ",".join(map(str, raw_input[0]))
-        data = {"body": f"[[{formatted_input}]]".replace(" ", "")}
+        data = {"body": f"[[{formatted_input}]]".replace(" ", "")} #raw_input.tolist()
 
         # Make a POST request to the API
         #response = requests.post(url=API_ENDPOINT, json=data)
@@ -96,9 +97,9 @@ def index(request):
 
 
         #phone_data_dict = '[[859,0,6,1,11,1,19,4,139,5,16,952,1713,2473,10,4,17,1,1,1]]'
-        flattened_list = str(phone_data_dict[0]).replace(" ", "")
+        #flattened_list = str(phone_data_dict[0]).replace(" ", "")
         # Wrapping in double square brackets to match the format
-        formatted_string = f"[{flattened_list}]"
+        #formatted_string = f"[{flattened_list}]"
 
         #data = {"body": formatted_string}
 
@@ -110,18 +111,29 @@ def index(request):
             if response.status_code == 200:
                 print(response.json())
                 result = response.json()
+                print(result)
+                body = json.loads(result['body'])
+                print(body)
+                if "Prediction" in body:
+                    finalResult = body["Prediction"]
+
+                if finalResult == "Low Cost":
+                    finalResult = "$50 - $150"
+                elif finalResult == "Medium Cost":
+                    finalResult = "$150 - $400"
+                elif finalResult == "High Cost":
+                    finalResult = "$400 - $700"
+                elif finalResult == "Very High Cost":
+                    finalResult = "$700 and above"
+
             else:
                 result = f"Error: {response.status_code}, {response.text}"
         except requests.exceptions.RequestException as e:
                 result = f"Request failed: {str(e)}"
 
-    if result == "Low Cost":
-        result = "$50 - $150"
-    elif result == "Medium Cost":
-        result = "$150 - $400"
-    elif result == "High Cost":
-        result = "$400 - $700"
-    elif result == "Very High Cost":
-        result = "$700 and above"
+    if finalResult != "":
+        result = finalResult
+
+
 
     return render(request, "PricePrediction/index.html", {"result": result})
